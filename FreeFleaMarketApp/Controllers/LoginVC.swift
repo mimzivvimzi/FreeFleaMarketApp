818ae58635b0a57ef926fab8d45df9fec6040b2e
Michelle Lau <mdeniselau@gmail.com>
2020-01-21 15:00:47 -0800

    First commit with only the view controllers

---
+++ FreeFleaMarketApp/Controllers/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginVC.swift
+//  FreeFleaMarketApp
+//
+//  Created by Michelle Lau on 2020/01/21.
+//  Copyright © 2020 Michelle Lau. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ FreeFleaMarketApp/Controllers/WelcomeVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FreeFleaMarketApp
+//
+//  Created by Michelle Lau on 2020/01/21.
+//  Copyright © 2020 Michelle Lau. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
