510020c195a59e325bc49d4c2fd3df839fe1a557
Michelle Lau <mdeniselau@gmail.com>
2020-02-03 22:57:02 +0900

    FirebaseUI connected.  Saves login info, but crashes.

--- FreeFleaMarketApp/Controllers/WelcomeVC.swift
+++ FreeFleaMarketApp/Controllers/WelcomeVC.swift
@@ -7,59 +7,16 @@
 //
 
 import UIKit
-import Firebase
-import FirebaseAuth
 import FirebaseUI
 
 class WelcomeVC: UIViewController {
     
-    // ADDED HANDLER FROM EXAMPLE CODE
-    var handle: AuthStateDidChangeListenerHandle?
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    // ADDED THIS FROM THE EXAMPLE CODE.  WHAT SHOULD I PUT INSTEAD OF self.tableView.reloadData()?
-    override func viewWillAppear(_ animated: Bool) {
-      super.viewWillAppear(animated)
-      // [START auth_listener]
-      handle = Auth.auth().addStateDidChangeListener { (auth, user) in
-        // [START_EXCLUDE]
-        self.setTitleDisplay(user)
-        //self.tableView.reloadData()
-        // [END_EXCLUDE]
-      }
-    }
-    
-    func setTitleDisplay(_ user: User?) {
-      if let name = user?.displayName {
-        self.navigationItem.title = "Welcome \(name)"
-      } else {
-        self.navigationItem.title = "Authentication Example"
-      }
-    }
-    
-    
-    
-//    override func viewWillAppear(_ animated: Bool) {
-//        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
-//          // ...
-//        }
-//    }
-//
-//    override func viewWillDisappear(_ animated: Bool) {
-//        Auth.auth().removeStateDidChangeListener(handle!)
-//
-//    }
-    
-    
-    // NOT SURE HOW TO USE THIS
-//    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
-//      // handle user and error as necessary
-//    }
 
 
     @IBAction func loginPressed(_ sender: UIButton) {
@@ -70,10 +27,12 @@ class WelcomeVC: UIViewController {
         guard authUI != nil else {
             // LOG THE ERROR
             return
-        } // HOW DO YOU READ THIS IN PLAIN ENGLISH?  MAKE SURE authUI IS NOT NIL?
+        } // CHECK THAT authUI IS NOT NIL
         
         // SET SELF AS THE DELEGATE
         authUI?.delegate = self
+        authUI?.providers = [FUIEmailAuth()]
+
         
         // GET A REFERENCE TO THE AUTH UI VIEW CONTROLLER
         let authViewController = authUI!.authViewController()
