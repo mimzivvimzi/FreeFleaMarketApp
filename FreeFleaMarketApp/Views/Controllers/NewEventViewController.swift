a2d67033a81b133b986044ab2ca54353940fa141
Michelle Lau <mdeniselau@gmail.com>
2020-03-25 23:16:57 +0900

    Change to static cells in NewEventViewController

--- FreeFleaMarketApp/Views/Controllers/NewEventViewController.swift
+++ FreeFleaMarketApp/Views/Controllers/NewEventViewController.swift
@@ -1,22 +1,24 @@
 //
-//  NewEventVC.swift
+//  NewEvent.swift
 //  FreeFleaMarketApp
 //
-//  Created by Michelle Lau on 2020/01/27.
+//  Created by Michelle Lau on 2020/03/25.
 //  Copyright © 2020 Michelle Lau. All rights reserved.
 //
+
 
 import UIKit
 import Firebase
 
-class NewEventViewController: UIViewController {
+class NewEventViewController: UITableViewController {
     
-    
+
     @IBOutlet weak var titleField: UITextField!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var dateField: UITextField!
     @IBOutlet weak var locationField: UITextField!
     @IBOutlet weak var descriptionField: UITextField!
+    
     let db = Firestore.firestore()
     let postID = UUID().uuidString
     
@@ -32,7 +34,7 @@ class NewEventViewController: UIViewController {
         let dateToString = dateFormatter.string(from: datePicker.date)
         dateField.text = dateToString
     }
-    
+
     @IBAction func saveEvent(_ sender: UIButton) {
         if Auth.auth().currentUser != nil {
             let ref = Database.database().reference()
@@ -47,7 +49,7 @@ class NewEventViewController: UIViewController {
                              "details": newEvent.details] as [String : Any]
             ref.child("posts").child("\(postID)").setValue(eventPost)
             self.navigationController?.popViewController(animated: true)
-//            self.presentingViewController?.dismiss(animated: true, completion: nil)
+    //            self.presentingViewController?.dismiss(animated: true, completion: nil)
         } else {
           print("No one is signed in")
         }
